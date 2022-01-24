637d7514c2ca1cf1b7f0f97f011420c352107a5a
YashSubhash <90604617+YashSubhash@users.noreply.github.com>
2022-01-24 18:14:14 +0530

    Add files via upload

---
+++ spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"Kiraathaka",
+        rating:"5",
+        genres:"comedy",
+        comment:"good movie"
+    },
+    movie2:{
+        name:"spider man no way home",
+        rating:"4.8",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"KGF",
+        rating:"4.3",
+        genres:"action",
+        comment:"good movie"
+    },
+    movie4:{
+        name:"kalpana",
+        rating:"4.5",
+        genres:"horror",
+        comment:"Good"
+    },
+}
